d9875543b554338b3a9778d6f79c6a35fa61e58a
Ram Iyer <ramky.iyer@gmail.com>
2021-02-23 22:03:54 -0500

    adding options to set root password

--- environments/production/manifests/site.pp
+++ environments/production/manifests/site.pp
@@ -1,4 +1,10 @@
 node 'ip-172-31-71-184.ec2.internal' {
+  class { '::mysql::server':
+    root_password    => 'strongpassword',
+    override_options => {
+      'mysqld' => { 'max_connections' => '1024' }
+    }
+  }
   include mysql_server
 }
 
